35cfabb236d7670c15f2c0cec3c4020a9fe5bae8
Jaya Jagriti <jayajagriti@Jayas-MacBook-Air.local>
2026-04-22 11:38:48 +0530

    professional knowledge base

--- rag.py
+++ rag.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import FakeEmbeddings
 
 vector_store = None
 
@@ -19,7 +19,7 @@ def load_base_knowledge():
 
     chunks = splitter.split_documents(docs)
 
-    embeddings = HuggingFaceEmbeddings()
+    embeddings = FakeEmbeddings(size=1536)
 
     vector_store = FAISS.from_documents(chunks, embeddings)
 
